2311243f4c969ddc7dba762ae8c0a5d3a5e07ef4
Jün <jun@hjtech.tw>
2022-12-29 18:08:10 +0800

    fix: i18n locale path

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,24 +1,15 @@
 import { createI18n } from 'vue-i18n';
 import tw from './zh-tw.js';
-import en from './en-us.json';
+import en from './en.js';
 
 const i18n = new createI18n({
-	locale: 'tw',
+	locale: 'zh-tw',
+	fallbackLocale: tw, //當選擇的語系無對應翻譯時，則使用的備用語系
 	legacy: false,  // 讓你可以在 composition API 中使用
 	globalInjection: true, // 全域注入，讓你在 <template> 可以使用 $t
 	messages: {
 		'zh-tw': tw,
-		'en-US': en
-		// tw: {
-		// 	message: {
-		// 		hello: '你好'
-		// 	}
-		// },
-		// en: {
-		// 	message: {
-		// 		hello: 'hello'
-		// 	}
-		// }
+		en: en
 	}
 });
 
