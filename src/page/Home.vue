55ef99e7b07d1d697d4a3d8670d53b8e3a6659ef
Jumanji <keep9nine@gmail.com>
2023-06-04 17:23:01 +0800

    feat: adjust home__top layout

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -5,34 +5,40 @@
 <template>
   <BlockHeader />
   <main class="pt-12 capitalize">
-    <div class="flex flex-col items-start justify-between mx-auto transition-all home__top lg:container lg:px-6 lg:flex-row">
-      <!-- left -->
-      <section class="min-w-full lg:min-w-[40vw] xl:min-w-[32vw] p-4 md:pt-12 lg:pt-24 lg:pb-0 lg:pr-0 lg:pl-10 xl:pl-20 text-center text-white whitespace-pre-wrap lg:text-left">
-        <h1
-          class="mb-4 text-4xl font-bold sm:text-6xl "
-        >
-          {{ $t('home.title') }}
-        </h1>
-        <p class="mb-12 text-xl md:mb-16 sm:text-2xl">
-          {{ $t('home.content') }}
-        </p>
-        <router-link
-          v-slot="{ navigate }"
-          to="/Channel"
-          custom
-        >
-          <button
-            type="button"
-            class="btn btn-main min-w-[14rem] uppercase"
-            @click="navigate"
+    <article class="relative flex flex-col px-6 mx-auto transition-all lg:flex-row home__top lg:container">
+      <div class="lg:px-6 grow lg:max-xl:basis-1/2">
+        <!-- left -->
+        <section class="p-4 text-center text-white whitespace-pre-wrap md:pt-12 lg:pt-24 lg:pb-0 lg:pr-0 lg:pl-10 xl:pl-20 lg:text-left">
+          <h1
+            class="mb-4 text-4xl font-bold md:max-xl:text-5xl xl:text-6xl"
           >
-            {{ $t('home.ctaButton') }}
-          </button>
-        </router-link>
-      </section>
-      <!-- right -->
-      <div class="w-full h-[27rem] sm:h-[33rem] bg-[left_-68px_center] sm:bg-center lg:bg-right 2xl:bg-center bg-no-repeat bg-cover sm:bg-contain md:bg-auto bg-[url('assets/main_banenr.png')] " />
-    </div>
+            {{ $t('home.title') }}
+          </h1>
+          <p class="mb-12 text-xl md:mb-16 sm:text-2xl">
+            {{ $t('home.content') }}
+          </p>
+          <router-link
+            v-slot="{ navigate }"
+            to="/Channel"
+            custom
+          >
+            <button
+              type="button"
+              class="btn btn-main min-w-[14rem] uppercase"
+              @click="navigate"
+            >
+              {{ $t('home.ctaButton') }}
+            </button>
+          </router-link>
+        </section>
+      </div>
+      <div class="mx-auto 2xl:basis-1/2">
+        <img
+          src="@/assets/main_banenr.png"
+          alt="大標題的插圖"
+        >
+      </div>
+    </article>
   </main>
 </template>
 
