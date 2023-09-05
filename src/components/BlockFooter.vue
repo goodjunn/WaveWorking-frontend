2545597ca783161151849ac3bd400d7eb66aa79b
Jumanji <keep9nine@gmail.com>
2023-09-05 17:17:38 +0800

    feat: add footer block

---
+++ src/components/BlockFooter.vue
@@ -0,0 +1,31 @@
+<script setup>
+// const { t } = useI18n();
+
+</script>
+
+<template>
+  <footer class="border-t border-accent-500">
+    <article class="container relative flex flex-col items-center px-6 py-8 mx-auto gap-y-8 sm:gap-x-6 sm:flex-row ">
+      <div class="px-6 text-xl text-center text-primary-100 sm:text-2xl basis-3/5 sm:text-left">
+        {{ $t('footerContent') }}
+      </div>
+      <div class="flex items-center justify-center px-6 grow">
+        <button
+          type="button"
+          class="btn btn-main min-w-[14rem] uppercase"
+          @click="navigate"
+        >
+          {{ $t('home.ctaButton') }}
+        </button>
+      </div>
+      <img
+        src="@/assets/footer_circle_bg.png"
+        alt="circle background"
+        class="absolute -z-[1] object-cover max-w-[776px] -right-[20rem] -top-[15rem] md:-right-[10rem] md:-top-[18rem] mix-blend-hard-light"
+      >
+    </article>
+  </footer>
+</template>
+
+<style scoped>
+</style>
